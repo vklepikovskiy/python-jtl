3af630e079af6523d689fc11aaf0d8a5dafd16fb
Victor Klepikovskiy <vklepikovskiy@gmail.com>
2012-07-22 18:22:17 +0300

    Added unit tests

---
+++ tests/test_jtl.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2012 Victor Klepikovskiy
+#
+# This file is part of python-jtl.
+#
+# python-jtl is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# python-jtl is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-jtl. If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest
+import jtl
+import os.path
+
+
+class JTLTestCase(unittest.TestCase):
+    def test_http_samples(self):
+        tests_dir = os.path.dirname(os.path.abspath(__file__))
+        sample_filename = os.path.join(tests_dir, 'samples/1.jtl')
+        http_samples = list(jtl.Parser(sample_filename).http_samples())
+        self.assertEqual(len(http_samples), 80)
+        test_sample = jtl.Sample(
+                by='61870', de=None, dt='text', ec=None, hn=None, it=None,
+                lb='/', lt='796', na=None, ng=None, rc='200', rm='OK',
+                s='true', sc=None, t='1196', tn='Thread Group 1-1',
+                ts='1341523888836')
+        self.assertEqual(http_samples[0], test_sample)
+        test_sample = jtl.Sample(
+                by='2977', de=None, dt='text', ec=None, hn=None, it=None,
+                lb='/some_page', lt='105', na=None, ng=None, rc='404',
+                rm='Not Found', s='false', sc=None, t='105',
+                tn='Thread Group 1-2', ts='1341523917934')
+        self.assertEqual(http_samples[-1], test_sample)
+        test_sample = jtl.Sample(
+                by='20155', de=None, dt='text', ec=None, hn=None, it=None,
+                lb='/search/images;_ylt=A0oG7lg2AvZPowgACQNXNyoA', lt='504',
+                na=None, ng=None, rc='200', rm='OK', s='true', sc=None,
+                t='666', tn='Thread Group 1-2', ts='1341523903585')
+        self.assertEqual(http_samples[39], test_sample)
+
+
+if __name__ == '__main__':
+    unittest.main()
