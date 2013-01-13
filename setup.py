66cc4a27a1b6aa17e00303cb003a34603b5ac94a
Victor Klepikovskiy <vklepikovskiy@gmail.com>
2013-01-13 17:13:09 +0200

    Added function to setup.py for reading the contents of README.txt file

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 setup(name='python-jtl',
       version='0.1.0',
       description='Python module for parsing JMeter test results',
-      long_description=open('README.txt').read(),
+      long_description=read('README.txt'),
       author='Victor Klepikovskiy',
       author_email='vklepikovskiy@gmail.com',
       url='http://code.google.com/p/python-jtl/',
